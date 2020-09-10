8c50c409ccb909a01b678b0dc693f4b1ce9a368d
sysatom <sysatom@gmail.com>
2020-09-10 17:48:49 +0800

    feat: ex06

---
+++ ex06/ex06.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int distance = 100;
+    float power = 2.345f;
+    double super_power = 56789.4311;
+    char initial = 'a';
+    char first_name[] = "Zed";
+
+
+    printf("%d, %f, %f, %c, %s", distance, power, super_power, initial, first_name);
+
+    return 0;
+}
